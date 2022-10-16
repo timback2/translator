85f529cc925c6fa55746f9ca3d76b2b6eed0f231
Manav Leslie <manav.leslie@adarga.ai>
2022-10-16 07:42:59 +0100

    added detect langauge

--- src/generate_audio_file.py
+++ src/generate_audio_file.py
@@ -1,28 +1,51 @@
 import time
 from gtts import gTTS
 import argparse
+from langdetect import detect, DetectorFactory
 
 
-def generate_audio_file(text, filename):
-    tts = gTTS(text=text, lang='en')
+def generate_audio_file(text, language, filename):
+    tts = gTTS(text=text, lang=language)
     tts.save(filename)
     print("Generated audio file " + filename)
+
 
 def get_file_contents(filename):
     with open(filename) as f:
         return f.read()
 
-if __name__ == '__main__':
+
+def detect_language(text):
+    try:
+        DetectorFactory.seed = 0
+        return detect(text)
+
+    except:
+        return "unknown"
+
+
+if __name__ == "__main__":
     start_time = time.time()
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', type=str, default='input/data.txt', help='Path to the input data file.')
-    parser.add_argument('--output', type=str, default='output/audio.mp3', help='Path to the output audio directory.')
+    parser.add_argument(
+        "--input",
+        type=str,
+        default="input/data.txt",
+        help="Path to the input data file.",
+    )
+    parser.add_argument(
+        "--output",
+        type=str,
+        default="output/audio.mp3",
+        help="Path to the output audio directory.",
+    )
     args = parser.parse_args()
 
-
+    text = get_file_contents(args.input)
+    language = detect_language(text)
+    if language != 'unknown':
     # Generate the audio files
-    generate_audio_file(get_file_contents(args.input), args.output)
-
+        generate_audio_file(text, language, args.output)
 
     # Print the time taken
-    print(f'Time taken: {time.time() - start_time:.2f} seconds')
+    print(f"Time taken: {time.time() - start_time:.2f} seconds")
